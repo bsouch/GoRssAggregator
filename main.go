041a05b1a6985a7d8d48b10d6e7c06240f7bf05c
bsouch96 <bksouch96@gmail.com>
2025-06-10 22:44:15 +0100

    Added server logic. Server now listens for requests

--- main.go
+++ main.go
@@ -1,5 +1,33 @@
 package main
 
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/go-chi/chi/v5"
+	"github.com/joho/godotenv"
+)
+
 func main() {
-	print("Hello World")
+	godotenv.Load(".env")
+
+	port := os.Getenv("PORT")
+	if port == "" {
+		log.Fatal("PORT is not found in the environment")
+	}
+
+	fmt.Printf("Running Web Server on Port: %v", port)
+
+	router := chi.NewRouter()
+	server := &http.Server{
+		Handler: router,
+		Addr:    ":" + port,
+	}
+
+	err := server.ListenAndServe()
+	if err != nil {
+		log.Fatal(err)
+	}
 }
