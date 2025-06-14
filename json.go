d81343a6af9fc57fcff3bc8210dc70af82450bae
bsouch96 <bksouch96@gmail.com>
2025-06-14 20:08:38 +0100

    Added a Json convert function

---
+++ json.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"encoding/json"
+	"log"
+	"net/http"
+)
+
+func toJson(writer http.ResponseWriter, statusCode int, payload interface{}) {
+	data, err := json.Marshal(payload)
+	if err != nil {
+		log.Printf("Failed to Marshal Json response: %v", payload)
+		writer.WriteHeader(500)
+		return
+	}
+
+	writer.Header().Add("Content-Type", "application/json")
+	writer.WriteHeader(statusCode)
+	writer.Write(data)
+}
